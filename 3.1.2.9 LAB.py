a79b8315171e91ae67d31dd550acb4ad00d013fe
ehab1980 <71230316+ehab1980@users.noreply.github.com>
2020-09-20 21:40:27 +0300

    Add files via upload

---
+++ 3.1.2.9 LAB.py
@@ -0,0 +1,7 @@
+while True:
+    eh=input('Enter the Exit word  : ')
+    if eh == 'chupacabra':
+        print("You've successfully left the loop.")
+        break
+    else:
+        print('Try again wrong word!')
